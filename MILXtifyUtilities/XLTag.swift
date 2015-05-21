0af8a5f86f18880be6631319e832eda04f643e37
Taylor Franklin <tfranklin@us.ibm.com>
2015-05-21 15:45:45 -0500

    swapped out copyright headers with shorter versions

--- MILXtifyUtilities/XLTag.swift
+++ MILXtifyUtilities/XLTag.swift
@@ -1,21 +1,10 @@
-//
-//  XLTag.swift
-//  XtifyTesting
-//
-//  Created by Evan Compton on 5/20/15.
-
-/**************************************
-*
-*  Licensed Materials - Property of IBM
-*  © Copyright IBM Corporation 2015. All Rights Reserved.
-*  This sample program is provided AS IS and may be used, executed, copied and modified without royalty payment by customer
-*  (a) for its own instruction and study, (b) in order to develop applications designed to run with an IBM product,
-*  either for customer's own internal use or for redistribution by customer, as part of such an application, in customer's
-*  own products.
-*
-***************************************/
+/*
+Licensed Materials - Property of IBM
+© Copyright IBM Corporation 2015. All Rights Reserved.
+*/
 
 import Foundation
+
 /**
  * This class represents an Xtify Tag
  **/
